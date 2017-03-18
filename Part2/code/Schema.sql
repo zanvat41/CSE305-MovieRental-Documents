9139fa1b10ee2145c6dd03d48e0a5f21e1682f29
SeanP <pesce.sean@gmail.com>
2017-03-18 17:38:33 -0400

    Change Primary Key IDs to auto-incrementing INT values instead of strings Change Credit Card numers, Zip codes, and phone numbers to be stored as INTs or BIGINTs instead of strings

--- Part2/code/Schema.sql
+++ Part2/code/Schema.sql
@@ -6,15 +6,13 @@
 # Database Schema - Entities & Relationships
 
 # @TODO: Add triggers/procedures for each constraint in this schema (constraints parsed but not supported by MySQL)
-# @TODO: Use the AUTO_INCREMENT keyword for numeric primary keys (for more info, see: https://dev.mysql.com/doc/refman/5.7/en/example-auto-increment.html )
-# @TODO: Use INT(9) UNSIGNED ZEROFILL AUTO_INCREMENT for SSNs
 
 ##########################
 ######Entity Tables#######
 ##########################
 
 CREATE TABLE Person (
-	ID CHAR(9),	# This is a customer's ID and an employee's SSN
+	ID INT(9) UNSIGNED ZEROFILL AUTO_INCREMENT,	# This is a customer's ID and an employee's SSN
 	LastName VARCHAR(64),
 	FirstName VARCHAR(64),
 	Address VARCHAR(64),
@@ -28,46 +26,39 @@ CREATE TABLE Person (
 		'UT','VA','VT','WA','WI','WV','WY',
 		'AS','DC','FM','GU','MH','MP','PR','PW','VI', # Territories/federal districts
 		'AA','AE','AP'), # Military bases
-	Zip CHAR(5),
-	Phone CHAR(10),
+	Zip INT(5) UNSIGNED ZEROFILL,
+	Phone BIGINT(10) UNSIGNED ZEROFILL,
 	PRIMARY KEY (ID),
-	UNIQUE KEY NameAddress (LastName, FirstName, Address, City, State, Zip), # There won't be two people with the same name in the same house
-	CONSTRAINT chk_ID CHECK (ID RLIKE '^[0-9]{9}$'), # Check that ID is formatted as 9 numbers
-	CONSTRAINT chk_Zip CHECK (Zip RLIKE '^[0-9]{5}$'),
-	CONSTRAINT chk_Phone CHECK (Phone RLIKE '^[0-9]{10}$')
+	UNIQUE KEY NameAddress (LastName, FirstName, Address, City, State, Zip) # There won't be two people with the same name in the same house
 );
 
 CREATE TABLE Customer ( # IsA Person
-	ID CHAR(9),	# References Person(ID)
+	ID INT(9) UNSIGNED ZEROFILL AUTO_INCREMENT,	# References Person(ID)
 	Email VARCHAR(64) NOT NULL,
-	CreditCard CHAR(16) NOT NULL,
-	Rating INT DEFAULT 1, 	# Customer Rating starts low because it is based on usage; new customers have never rented a movie
+	CreditCard BIGINT(16) UNSIGNED ZEROFILL NOT NULL,
+	Rating INT UNSIGNED DEFAULT 1, 	# Customer Rating starts low because it is based on usage; new customers have never rented a movie
 	PRIMARY KEY (ID),
 	FOREIGN KEY (ID) REFERENCES Person(ID)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
 	# UNIQUE KEY (Email), # Don't allow multiple accounts tied to the same email address	# EDIT: or they can? Part1 solution says customers can have multiple accounts
-	CONSTRAINT chk_ID CHECK (ID RLIKE '^[0-9]{9}$'),  # Check that AccountID is formatted as 9 numbers
 	CONSTRAINT chk_Rating CHECK (Rating IN (1, 2, 3, 4, 5)),
-	CONSTRAINT chk_Email CHECK (Email LIKE '%_@_%._%'), # The '%' char checks for 0 or more chars; '_' checks for exactly 1 character
-	CONSTRAINT chk_CC CHECK (CreditCard RLIKE '^[0-9]{16}$') # Check that CC# is composed of only numbers
+	CONSTRAINT chk_Email CHECK (Email LIKE '%_@_%._%') # The '%' char checks for 0 or more chars; '_' checks for exactly 1 character
 );
 
 CREATE TABLE Account (
-	ID INT,
-	CustomerID CHAR(9) NOT NULL,	# Account has participation constraint and must belong to a customer (HasAccount relationship)
+	ID INT UNSIGNED AUTO_INCREMENT,
+	CustomerID INT(9) UNSIGNED ZEROFILL NOT NULL,	# Account has participation constraint and must belong to a customer (HasAccount relationship)
 	Subscription ENUM('Limited', 'Unlimited', 'Unlimited+', 'Unlimited++') NOT NULL DEFAULT 'Limited', # ENUM Domain
 	Created DATE,		# DATEs are formatted like so: '2000-12-31'
 	PRIMARY KEY (ID),
 	FOREIGN KEY (CustomerID) REFERENCES Customer(ID)
 		ON DELETE NO ACTION
-		ON UPDATE CASCADE,
-	CONSTRAINT chk_ID CHECK (ID >= 0),
-	CONSTRAINT chk_CustID CHECK (CustomerID RLIKE '^[0-9]{9}$')
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE Employee ( # IsA Person
-	SSN CHAR(9), # References Person(ID)
+	SSN INT(9) UNSIGNED ZEROFILL AUTO_INCREMENT, # References Person(ID)
 	StartDate DATE,	# Start of employment
 	Position ENUM('Manager', 'Customer Rep'),	# @TODO: Do we need to implement this attribute or is it all determined by permissions?
 	HourlyRate FLOAT NOT NULL DEFAULT 20.00,
@@ -75,48 +66,39 @@ CREATE TABLE Employee ( # IsA Person
 	FOREIGN KEY (SSN) REFERENCES Person(ID)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
-	CONSTRAINT chk_Pay CHECK (HourlyRate >= 9.00), # Make sure employees don't get paid below minimum wage @TODO: make min wage a constant?
-	CONSTRAINT chk_SSN CHECK (SSN RLIKE '^[0-9]{9}$') # Check that SSN is formatted as 9 numbers
+	CONSTRAINT chk_Pay CHECK (HourlyRate >= 9.00) # Make sure employees don't get paid below minimum wage @TODO: make min wage a constant?
 	# @TODO: On Employee INSERT/UPDATE, if SSN conflicts with some customer's account ID, change customer ID (?)
 	# @TODO: On Employee INSERT, grant permissions to determine whether the user is a customer rep or manager (?)
-	# @TODO: Make sure there is always at least 1 manager
 );
 
 CREATE TABLE Movie (
-	ID INT,
+	ID INT UNSIGNED AUTO_INCREMENT,
 	Title VARCHAR(64) NOT NULL,
 	Genre ENUM ('Comedy', 'Drama', 'Action', 'Foreign'), # ENUM acts as a domain
 	Fee FLOAT DEFAULT 0.00,
-	TotalCopies INT DEFAULT 0,
-	Rating INT,
+	TotalCopies INT UNSIGNED DEFAULT 0,
+	Rating INT UNSIGNED,
 	PRIMARY KEY (ID),
-	CONSTRAINT chk_ID CHECK (ID >= 0),
 	CONSTRAINT chk_Rating CHECK (Rating IN (1, 2, 3, 4, 5)), # CONSTRAINT acts as a domain
-	CONSTRAINT chk_TotalCopies CHECK (TotalCopies >= 0),
-	CONSTRAINT chk_AvailableCopies CHECK (AvailableCopies >= 0),
-	CONSTRAINT chk_AvailableVsTotalCopies CHECK (AvailableCopies <= TotalCopies),
 	CONSTRAINT chk_Fee CHECK (Fee >= 0.0)
 );
 
 CREATE TABLE Actor (
-	ID INT,
+	ID INT UNSIGNED AUTO_INCREMENT,
 	FirstName VARCHAR(64) NOT NULL,
 	LastName VARCHAR(64) NOT NULL,
 	Gender ENUM('M','F'), # ENUM acts as a domain
-	Age INT,
-	Rating INT,
+	Age INT UNSIGNED,
+	Rating INT UNSIGNED,
 	PRIMARY KEY (ID),
-	CONSTRAINT chk_ID CHECK (ID >= 0),
-	CONSTRAINT chk_Rating CHECK (Rating IN (1, 2, 3, 4, 5)), # CONSTRAINT acts as a domain
-	CONSTRAINT chk_Age CHECK (Age >= 0)
+	CONSTRAINT chk_Rating CHECK (Rating IN (1, 2, 3, 4, 5)) # CONSTRAINT acts as a domain
 );
 
 CREATE TABLE _Order (	# Apparently "Order" is a MySQL keyword...
-	ID INT,
+	ID INT UNSIGNED AUTO_INCREMENT,
 	OrderDate DATETIME NOT NULL,		# DATETIMEs are formatted like so: '2000-12-31 23:59:59'
 	ReturnDate DATETIME DEFAULT NULL,
 	PRIMARY KEY (ID),
-	CONSTRAINT chk_ID CHECK (ID >= 0),
 	CONSTRAINT chk_Dates CHECK (ReturnDate >= OrderDate)
 );
 
@@ -131,10 +113,10 @@ CREATE TABLE _Order (	# Apparently "Order" is a MySQL keyword...
 
 # Represents movies that have been rented by customers
 CREATE TABLE Rental (
-	OrderID INT,
-	AccountID INT,
-	MovieID INT,
-	EmployeeID CHAR(9),
+	OrderID INT UNSIGNED AUTO_INCREMENT,
+	AccountID INT UNSIGNED,
+	MovieID INT UNSIGNED,
+	EmployeeID INT(9) UNSIGNED ZEROFILL,
 	PRIMARY KEY (OrderID, AccountID, MovieID, EmployeeID),
 	FOREIGN KEY (OrderID) REFERENCES _Order(ID)
 		ON DELETE NO ACTION
@@ -147,16 +129,15 @@ CREATE TABLE Rental (
 		ON UPDATE CASCADE,
 	FOREIGN KEY (EmployeeID) REFERENCES Employee(SSN)
 		ON DELETE NO ACTION		# Grader correction from part 1
-		ON UPDATE CASCADE,
-	CONSTRAINT chk_EmployeeID CHECK (EmployeeID RLIKE '^[0-9]{9}$')
+		ON UPDATE CASCADE
 	# @TODO: Each Order can only have 1 Rental associated with it
 );
 
 
 # Represents movies that users have added to their Movie Queue (aka Wishlist)
 CREATE TABLE Queued (
-	AccountID INT,
-	MovieID INT,
+	AccountID INT UNSIGNED,
+	MovieID INT UNSIGNED,
 	DateAdded DATETIME,	# Used to sort movies in the queue
 	PRIMARY KEY (AccountID, MovieID),
 	FOREIGN KEY (AccountID) REFERENCES Account(ID)
@@ -171,8 +152,8 @@ CREATE TABLE Queued (
 # Represents a relationship between an actor and a movie they have been
 #  casted in.
 CREATE TABLE Casted (
-	ActorID INT,
-	MovieID INT,
+	ActorID INT UNSIGNED,
+	MovieID INT UNSIGNED,
 	Role VARCHAR(128),
 	PRIMARY KEY (ActorID, MovieID),
 	FOREIGN KEY (ActorID) REFERENCES Actor(ID)
