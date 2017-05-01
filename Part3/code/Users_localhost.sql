2bd0b7e1194cf74c9e6488c47a85b8c8ea4f0164
SeanPesce <pesce.sean@gmail.com>
2017-05-01 10:27:16 -0400

    Move localhost user definitions to /code/ subdirectory

---
+++ Part3/code/Users_localhost.sql
@@ -0,0 +1,15 @@
+# Zhe Lin 109369879
+# Sean Pesce 107102508
+# Weichao Zhao 109957656
+#
+# CSE305 Database Project Part 3
+# Database User Creation Statement Compilation
+
+# This file can be copied and pasted directly into mysql to create the users from the demo data and grant them the correct privileges.
+
+# Employee usernames are first initial and last name
+# Employee passwords are last initial and first name
+
+CREATE USER 'dwarren'@'localhost' IDENTIFIED BY 'wdavid';
+
+
